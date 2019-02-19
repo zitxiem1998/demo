7f630723e23f4c36790b81dcd9eca58d7840be2d
zitxiem1998 <zitxiem1998@gmail.com>
2019-02-19 22:25:57 +0700

    hello

---
+++ 01.JavaBackend/01.JavaProgramming/Hello/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String name;
+        System.out.print("Enter your name: ");
+        name=scanner.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
