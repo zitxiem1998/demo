1ba0a2f2cf2ceb48b16d0279beff4a9804e7ce10
zitxiem1998 <zitxiem1998@gmail.com>
2019-02-20 14:58:24 +0700

    hien thi cac loai hinh

--- 01.JavaBackend/01.JavaProgramming/Perform/src/Main.java
+++ 01.JavaBackend/01.JavaProgramming/Perform/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     Scanner scanner = new Scanner(System.in);
 
-    public void Chunhat() {
+    public void rectangle() {
         int cdai, crong;
         System.out.print("Moi ban nhap chieu dai: ");
         cdai = scanner.nextInt();
@@ -17,7 +17,7 @@ public class Main {
         }
     }
 
-    public void Tamgiaccan() {
+    public void triangle() {
         int canh;
         System.out.print("Moi ban nhap canh cua tam giac vuong: ");
         canh = scanner.nextInt();
@@ -32,10 +32,13 @@ public class Main {
             }
         }
         if (lchon == 2) {
-            for (int i = 1; i<=canh;i++)
-            {
-                for(int j=canh;j>canh-i;j--)
+            for (int i = 1; i <= canh; i++) {
+                for (int k = 1; k < i; k++) {
+                    System.out.print(" ");
+                }
+                for (int j = canh - i; j >= 0; j--) {
                     System.out.print("*");
+                }
                 System.out.print("\r\n");
             }
         }
@@ -46,10 +49,38 @@ public class Main {
                 System.out.print("\r\n");
             }
         }
+        if (lchon == 4) {
+            for (int i = 1; i <= canh; i++) {
+                for (int j = canh; j >= i; j--) {
+                    System.out.print(" ");
+                }
+                for (int k = canh; k > canh - i; k--) {
+                    System.out.print("*");
+                }
+                System.out.print("\r\n");
+            }
+        }
     }
+    public void isosceles() {
+        int high;
+        System.out.println("Moi ban nhap duong cao cua tam giac: ");
+        high = scanner.nextInt();
 
+        for (int i = 0; i <= high; i++) {
+            for (int j = high; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = high; k > high - i; k--) {
+                System.out.print("*");
+            }
+            for(int j=high;j<=high+i;j++) {
+                System.out.print("*");
+            }
+            System.out.print("\r\n");
+        }
+    }
     public static void main(String[] args) {
         Main main = new Main();
-        main.Tamgiaccan();
+        main.isosceles();
     }
 }
